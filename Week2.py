0ecf50f92691d414ca81ece3bc268568f3ea90af
Asim-Dhakal <asimdhakal444@gmail.com>
2024-10-04 15:27:12 +0100

    Final

--- Week2.py
+++ Week2.py
@@ -1,22 +1,32 @@
 import unittest
 
-# the function to be tested
+# the function to check if input is even 
+
 def NumberIsEven(Number):
+    if type(Number) != int:
+        return False
+
     if Number % 2 == 0:
         return True 
     else:
         return False 
 
 class TestFunction(unittest.TestCase):
+
     #tests with Odd number
     def test_Odd_number(self):
         result = NumberIsEven(5)
         self.assertEqual(result, False)
+
     # tests with Even number 
     def test_Even_number(self):
         result = NumberIsEven(4)
         self.assertEqual(result, True)
-    
 
+    # tests with Odd number 
+    def test_None_number(self):
+        result = NumberIsEven('String')
+        self.assertEqual(result, False)
+        
 if __name__ == '__main__':
     unittest.main()
