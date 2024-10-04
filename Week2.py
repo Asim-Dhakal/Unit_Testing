d2e804d41f754c4e6b936d11496d3cb0bd8f2fc4
Asim-Dhakal <asimdhakal444@gmail.com>
2024-10-04 15:21:20 +0100

    Checks if it working proper or not

--- Week2.py
+++ Week2.py
@@ -1,6 +1,22 @@
+import unittest
+
+# the function to be tested
 def NumberIsEven(Number):
     if Number % 2 == 0:
         return True 
     else:
         return False 
-print(NumberIsEven(2))
+
+class TestFunction(unittest.TestCase):
+    #tests with Odd number
+    def test_Odd_number(self):
+        result = NumberIsEven(5)
+        self.assertEqual(result, False)
+    # tests with Even number 
+    def test_Even_number(self):
+        result = NumberIsEven(4)
+        self.assertEqual(result, True)
+    
+
+if __name__ == '__main__':
+    unittest.main()
